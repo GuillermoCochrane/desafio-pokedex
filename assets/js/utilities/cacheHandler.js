4c4dc19feb83eef6e2e7e81848d8ccc25bd95848
GuillermoCochrane <guilleac81@gmail.com>
2025-10-31 19:47:51 -0300

    Agregado saveCacheData, que guarda datos en localStorage

--- assets/js/utilities/cacheHandler.js
+++ assets/js/utilities/cacheHandler.js
@@ -8,7 +8,7 @@ function cacheErrorHandler(type= "GENERIC_ERROR", message= "Ha ocurrido un error
   return error;
 }
 
-// Como el throw funciona como return, no implementamos el try / catch, ya que el manejo lo hacemos en la funcion que consume los helpers (DRY logic)
+//? Como el throw funciona como return, no implementamos el try / catch, ya que el manejo lo hacemos en la funcion que consume los helpers (DRY logic)
 
 // Recupera datos guardados en localStorage
 export function loadCacheData(key = null) {
@@ -18,4 +18,13 @@ export function loadCacheData(key = null) {
   if (!savedData) throw cacheErrorHandler("MISSING_CACHE", `No hay datos almacenados en: ${key}`, key);
   
   return JSON.parse(savedData);
+}
+
+// Guarda datos en localStorage
+export function saveCacheData(data = null, key = null) {
+  if (!data) throw cacheErrorHandler("VALIDATION_ERROR", "Se requieren los datos a guardar", null, data);
+  if (!key) throw cacheErrorHandler("VALIDATION_ERROR", "Se requiere una clave para guardar los datos", key);
+
+  localStorage.setItem(key, JSON.stringify(data));
+  return true;
 }
