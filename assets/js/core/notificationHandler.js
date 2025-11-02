cbe8e9e8561a90582b736bf62312a80eab0729b4
GuillermoCochrane <guilleac81@gmail.com>
2025-11-02 19:39:08 -0300

    Agregado showNextNotification,  handler de la cola de notificaciones

--- assets/js/core/notificationHandler.js
+++ assets/js/core/notificationHandler.js
@@ -10,3 +10,27 @@ export function showNotification(message, type = 'info') {
     showNextNotification(); // si no está mostrando una notificación, la mostramos
   }
 }
+
+// Handler de la cola de notificaciones
+function showNextNotification() {
+  if (notificationQueue.length === 0) { // Si la cola está vacía, no mostramos nada
+    isShowingNotification = false; //Seteamos el flag de mostrar notificación a falso y salimos
+    return;
+  }
+  
+  isShowingNotification = true; //Seteamos el flag de mostrar notificación a verdadero
+  const { message, type } = notificationQueue.shift(); //Tomamos la primera notificación de la cola
+  const $notification = $('#notification');
+
+  $notification.textContent = message;
+  $notification.style.borderLeftColor = `var(--bs-${type})`;
+  $notification.classList.add('show'); // ← Mostramos la notificación
+  
+  setTimeout(() => {
+    $notification.classList.remove('show'); // ← Ocultamos la notificación despues de 3 segundos
+    
+    setTimeout(() => {
+      showNextNotification(); // Volvemos a ejecutarla, por si queda alguna notificación en la cola
+    }, 300); 
+  }, 3000);
+}
