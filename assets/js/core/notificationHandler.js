fc8c213ab1ad50694062e5b3dca2cb680c3167af
GuillermoCochrane <guilleac81@gmail.com>
2025-11-02 19:38:18 -0300

    showNotification modificado, para implmenetar sistema de cola de notificaciones

--- assets/js/core/notificationHandler.js
+++ assets/js/core/notificationHandler.js
@@ -1,14 +1,12 @@
 // Handler de notificaciones
 import { $ } from '../utilities/dom.js';
+let notificationQueue = []; // Cola de notificaciones
+let isShowingNotification = false; // Flag para mostrar notificaciones
 
+// Muestra una notificación, o agrega una a la cola
 export function showNotification(message, type = 'info') {
-  const $notification = $('#notification');
-
-  $notification.textContent = message;
-  $notification.style.borderLeftColor = `var(--bs-${type})`;
-  $notification.classList.add('show');
-  
-  setTimeout(() => {
-    $notification.classList.remove('show');
-  }, 3000);
+  notificationQueue.push({ message, type }); //Agregarmos la notificación a la cola
+  if (!isShowingNotification) {
+    showNextNotification(); // si no está mostrando una notificación, la mostramos
+  }
 }
